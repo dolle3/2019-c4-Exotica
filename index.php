420deba19372b953a72b9e55e461a51e354b3a55
dolle3 <larsvandoremalen@gmail.com>
2019-09-23 10:53:06 +0200

    0.0.9

--- index.php
+++ index.php
@@ -1,9 +1,46 @@
 <?php
-require_once 'core/init.php';
+  session_start();
 
-$lars = new User('kees', '1');
-$log = new Login('1', 'hasht',"hoi");
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
 
-echo 'de naam is: ' .$lars->getUsername(). '<br>';
-echo 'het id is: ' .$lars->getId(). '<br>';
-echo 'dit is het Id: '.$log->getId(). ', Password: '.$log->getPassword().', de username is: ' .$lars->getUsername();
+<div class="header">
+	<h2>Home Page</h2>
+</div>
+<div class="content">
+  	<!-- notification message -->
+  	<?php if (isset($_SESSION['success'])) : ?>
+      <div class="error success" >
+      	<h3>
+          <?php
+          	echo $_SESSION['success'];
+          	unset($_SESSION['success']);
+          ?>
+      	</h3>
+      </div>
+  	<?php endif ?>
+
+    <!-- logged in user information -->
+    <?php  if (isset($_SESSION['username'])) : ?>
+    	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    <?php endif ?>
+</div>
+
+</body>
+</html>
