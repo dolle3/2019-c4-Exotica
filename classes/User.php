1e4d16a971702c91f2399a1e71de4fb695bd52a2
dolle3 <larsvandoremalen@gmail.com>
2019-09-23 10:21:20 +0200

    0.0.7

--- classes/User.php
+++ classes/User.php
@@ -1,4 +1,22 @@
 <?php
-class User{
 
+class User
+{
+    private $_Username;
+    private $_Id;
+
+    public function __construct($name, $id)
+    {
+        $this->_Username = $name;
+        $this->_Id = $id;
+    }
+
+    public function getUsername()
+    {
+        return $this->_Username;
+    }
+
+    public function getId() {
+        return $this->_Id;
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
-require_once '../core/init.php';
+require_once 'core/init.php';
 
+$lars = new User('kees', '1');
+
+echo 'de naam is: ' .$lars->getUsername(). '<br>';
+echo 'het id is: ' .$lars->getId(). '<br>';
