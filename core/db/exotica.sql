1894b8b82de84ec8ca75b06e61612c7db4a0ba70
olaf majoor <323666@student.mboutrecht.nl>
2020-01-31 12:27:59 +0100

    0.0.44

--- core/db/exotica.sql
+++ core/db/exotica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 18 jan 2020 om 12:41
+-- Gegenereerd op: 31 jan 2020 om 11:26
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -48,14 +48,43 @@ CREATE TABLE IF NOT EXISTS `agenda` (
 DROP TABLE IF EXISTS `factuur`;
 CREATE TABLE IF NOT EXISTS `factuur` (
   `factuurcode` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `userid` int(11) UNSIGNED NOT NULL,
-  `productid` int(11) UNSIGNED NOT NULL,
+  `userid` int(11) UNSIGNED DEFAULT NULL,
   `factuurdatum` timestamp NOT NULL,
-  `aantal` int(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`factuurcode`),
-  KEY `productid to product_idx` (`productid`),
   KEY `userid to user_idx` (`userid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `factuur`
+--
+
+INSERT INTO `factuur` (`factuurcode`, `userid`, `factuurdatum`) VALUES
+(11, NULL, '2020-01-28 14:06:47');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `orderline`
+--
+
+DROP TABLE IF EXISTS `orderline`;
+CREATE TABLE IF NOT EXISTS `orderline` (
+  `idorderline` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `factuurcode` int(11) UNSIGNED NOT NULL,
+  `productid` int(11) UNSIGNED NOT NULL,
+  `aantal` int(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`idorderline`),
+  KEY `factuurcode to factuur_idx` (`factuurcode`,`productid`),
+  KEY `product id to product_idx` (`productid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `orderline`
+--
+
+INSERT INTO `orderline` (`idorderline`, `factuurcode`, `productid`, `aantal`) VALUES
+(3, 11, 2, 1),
+(4, 11, 1, 4);
 
 -- --------------------------------------------------------
 
@@ -82,8 +111,8 @@ CREATE TABLE IF NOT EXISTS `product` (
 --
 
 INSERT INTO `product` (`productid`, `productcode`, `productnaam`, `beschrijving`, `prijs`, `productaantal`, `image`, `productcategorieid`) VALUES
-(1, 'EXO123', 'Exo Terra Glas Terrarium Zwart - Terraria - 60x45x60 cm', 'Het Exo Terra Glasterrarium is de ideale behuizing voor reptielen en amfibieën ontworpen door Europese herpetologen. De vooraan openende deuren vergemakkelijken het voeren en onderhoud. Een speciaal ontworpen sluiting voorkomt ontsnappen en de deuren kunnen afzonderlijk geopend worden. De volledig uit staalgaas bestaande bovenzijde laat UVB en infrarood door, en kan in zijn geheel worden weggenomen en vergemakkelijkt het inrichten en onderhoud. Aan beide achterzijden van het draadrooster zijn vijf afsluitbare ingangen voor snoeren en buizen voor het gebruik in het terrarium van Heat Wave Rocks, Waterfalls, sensoren, enz.. Door de verheven terrariumbodem kan een substraatverwarmer onderaan worden aangebracht om een deel van het grondoppervlak te verwarmen. De extra hoge voorruit is ideaal voor een dikke bodembedekking (gravende reptielen), een watergedeelte (paludarium) of het aanbrengen van een Exo Terra Reptile Den. De natuurlijke achterwand is in dezelfde kleur als de andere Exo Terra decoratiematerialen (Water Dishes, Waterfalls, Heat Wave Rocks, enz.) en biedt extra klimmogelijkheden.', '179.00', 100, 'img/apparatuur/aquarium1.png', 4),
-(2, 'EXO-5364', 'Paludarium Terrarium - 30 x 30 x 60 cm', 'Dit nieuwe type extra hoog Exo Terra glazen terrarium is ideaal voor boombewonende reptielen- en amfibieënsoorten. De unieke achtergrond met wortelmotief maakt er een geweldig habitat van voor een groot aantal boombewonende soorten zoals boomboa’s en pythons, daggekko’s, bladstaartgekko’s, tokehs, boomkikkers enz. Het heeft alle andere goede kenmerken van de Exo Terra glazen terrariums; Het Exo Terra glasterrarium is de ideale behuizing voor reptielen en amfibieën ontworpen door Europese herpetologen. De vooraan openende deuren vergemakkelijken het voeren en onderhoud. Een speciaal ontworpen sluiting voorkomt ontsnappen en de deuren kunnen afzonderlijk geopend worden. De volledig uit staalgaas bestaande bovenzijde laat UVB en infrarood door, en kan in zijn geheel worden weggenomen en vergemakkelijkt het inrichten en onderhoud. Aan beide achterzijden van het draadrooster zijn vijf afsluitbare ingangen voor snoeren en buizen voor het gebruik in het terrarium van Heat Wave Rocks, Waterfalls, sensoren en dergelijke. Door de verheven terrariumbodem kan een substraatverwarmer onderaan worden aangebracht om een deel van het grondoppervlak te verwarmen. De extra hoge voorruit is ideaal voor een dikke bodembedekking (gravende reptielen), een watergedeelte (paludarium) of het aanbrengen van een Exo Terra Reptile Den. De natuurlijke achterwand is in dezelfde kleur als de andere Exo Terra decoratiematerialen (Water Dishes, Waterfalls, Heat Wave Rocks en meer) en biedt extra klimmogelijkheden. Afmetingen: 30 x 30 x 60 cm Merk Exo Terra Samenvattend Waterdichte bodem.', '99.99', 100, 'img/apparatuur/aquarium2.png', 4);
+(1, 'EXO123', 'Exo Terra Glas Terrarium Zwart - Terraria - 60x45x60 cm', 'Het Exo Terra Glasterrarium is de ideale behuizing voor reptielen en amfibieën ontworpen door Europese herpetologen. De vooraan openende deuren vergemakkelijken het voeren en onderhoud. Een speciaal ontworpen sluiting voorkomt ontsnappen en de deuren kunnen afzonderlijk geopend worden. De volledig uit staalgaas bestaande bovenzijde laat UVB en infrarood door, en kan in zijn geheel worden weggenomen en vergemakkelijkt het inrichten en onderhoud. Aan beide achterzijden van het draadrooster zijn vijf afsluitbare ingangen voor snoeren en buizen voor het gebruik in het terrarium van Heat Wave Rocks, Waterfalls, sensoren, enz.. Door de verheven terrariumbodem kan een substraatverwarmer onderaan worden aangebracht om een deel van het grondoppervlak te verwarmen. De extra hoge voorruit is ideaal voor een dikke bodembedekking (gravende reptielen), een watergedeelte (paludarium) of het aanbrengen van een Exo Terra Reptile Den. De natuurlijke achterwand is in dezelfde kleur als de andere Exo Terra decoratiematerialen (Water Dishes, Waterfalls, Heat Wave Rocks, enz.) en biedt extra klimmogelijkheden.', '179.00', 76, 'img/apparatuur/aquarium1.png', 4),
+(2, 'EXO-5364', 'Paludarium Terrarium - 30 x 30 x 60 cm', 'Dit nieuwe type extra hoog Exo Terra glazen terrarium is ideaal voor boombewonende reptielen- en amfibieënsoorten. De unieke achtergrond met wortelmotief maakt er een geweldig habitat van voor een groot aantal boombewonende soorten zoals boomboa’s en pythons, daggekko’s, bladstaartgekko’s, tokehs, boomkikkers enz. Het heeft alle andere goede kenmerken van de Exo Terra glazen terrariums; Het Exo Terra glasterrarium is de ideale behuizing voor reptielen en amfibieën ontworpen door Europese herpetologen. De vooraan openende deuren vergemakkelijken het voeren en onderhoud. Een speciaal ontworpen sluiting voorkomt ontsnappen en de deuren kunnen afzonderlijk geopend worden. De volledig uit staalgaas bestaande bovenzijde laat UVB en infrarood door, en kan in zijn geheel worden weggenomen en vergemakkelijkt het inrichten en onderhoud. Aan beide achterzijden van het draadrooster zijn vijf afsluitbare ingangen voor snoeren en buizen voor het gebruik in het terrarium van Heat Wave Rocks, Waterfalls, sensoren en dergelijke. Door de verheven terrariumbodem kan een substraatverwarmer onderaan worden aangebracht om een deel van het grondoppervlak te verwarmen. De extra hoge voorruit is ideaal voor een dikke bodembedekking (gravende reptielen), een watergedeelte (paludarium) of het aanbrengen van een Exo Terra Reptile Den. De natuurlijke achterwand is in dezelfde kleur als de andere Exo Terra decoratiematerialen (Water Dishes, Waterfalls, Heat Wave Rocks en meer) en biedt extra klimmogelijkheden. Afmetingen: 30 x 30 x 60 cm Merk Exo Terra Samenvattend Waterdichte bodem.', '99.99', 84, 'img/apparatuur/aquarium2.png', 4);
 
 -- --------------------------------------------------------
 
@@ -130,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `employeeId` int(11) UNSIGNED NOT NULL,
   `username` varchar(100) NOT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `user`
@@ -154,8 +183,14 @@ ALTER TABLE `agenda`
 -- Beperkingen voor tabel `factuur`
 --
 ALTER TABLE `factuur`
-  ADD CONSTRAINT `productid to producttable` FOREIGN KEY (`productid`) REFERENCES `product` (`productid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `userid to user` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Beperkingen voor tabel `orderline`
+--
+ALTER TABLE `orderline`
+  ADD CONSTRAINT `factuurcode to factuur` FOREIGN KEY (`factuurcode`) REFERENCES `factuur` (`factuurcode`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `product id to product` FOREIGN KEY (`productid`) REFERENCES `product` (`productid`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Beperkingen voor tabel `product`
--- includes/checkout.php
+++ includes/checkout.php
@@ -1,24 +1,94 @@
 <?php
 require_once "./core/db/connectdb.php";
-if( isset($_SESSION['username']))
+if( isset($_SESSION['userId']))
 {
+    $userId = $_SESSION['userId'];
+    
+
+
+    
+        $tot = 0;
+        foreach ($_SESSION["shopping_cart"] as $product)
+        {
+            $product["prijs"];
+            $product["quantity"]; 
+            $tot += ($product["prijs"] * $product["quantity"]);
+            
+        
+        }
+        
+            $price_inc = $tot ;
+            $price_ex = $tot * 0.79;
+    
+        
+        $sql = "SELECT * FROM `factuur`";
+        
+        
+        $result = mysqli_query($con, $sql);
+        
+        
+        $sql = "INSERT INTO `factuur` (`factuurcode`,
+                                  `userid`,
+                                  `factuurdatum`
+                                  ) 
+             VALUES                (NULL, 
+                                    $userId,
+                                    NOW()
+                                    )"; 
+    
+    $result = mysqli_query($con, $sql );
+    
+     echo $sql;
+    
+    $factuurcode = mysqli_insert_id($con);
+    
+    foreach ($_SESSION["shopping_cart"] as $product){
+        $quantity = $product['quantity'];
+        $productId = $product['productid'];
+        
+    
+    $sql = "INSERT INTO `orderline` (`idorderline`, 
+                                     `factuurcode`, 
+                                     `productid`,  
+                                     `aantal` 
+                                     ) 
+             VALUES                 (NULL, 
+                                     '$factuurcode', 
+                                     '$productId', 
+                                     '$quantity'
+                                     )";
+                                     
+                                     
+    
+    
+    $result = mysqli_query($con, $sql);
+    
+    echo $sql;
+    
+    $sql2 = "SELECT * FROM product WHERE productid = '$productId'";
+    
+          $result2 = mysqli_query($con, $sql2);
+          $record2 = mysqli_fetch_assoc($result2);
+    
+          $af = $record2["productaantal"] - $quantity;
+    
+          $sql = "UPDATE product SET productaantal = '$af' WHERE product.productid = '$productId'";
+          mysqli_query($con,$sql);
+    
+    }
+}else{
     $tot = 0;
     foreach ($_SESSION["shopping_cart"] as $product)
     {
         $product["prijs"];
         $product["quantity"]; 
         $tot += ($product["prijs"] * $product["quantity"]);
-        $productId = $product['productid'];
-        $quantity = $product['quantity'];
+        
     
     }
     
         $price_inc = $tot ;
         $price_ex = $tot * 0.79;
-
-        $userId = $_SESSION['userId'];
-    
-
 
     
     $sql = "SELECT * FROM `factuur`";
@@ -29,20 +99,41 @@ if( isset($_SESSION['username']))
     
     $sql = "INSERT INTO `factuur` (`factuurcode`,
                               `userid`,
-                              `productid`,
-                              `factuurdatum`,
-                              `aantal`) 
+                              `factuurdatum`
+                              ) 
          VALUES                (NULL, 
-                                '$userId',
-                                '$productId',
-                                NOW(), 
-                                '$quantity')"; 
+                                NULL,
+                                NOW()
+                                )"; 
 
 $result = mysqli_query($con, $sql );
 
+ echo $sql;
+
+$factuurcode = mysqli_insert_id($con);
+
+foreach ($_SESSION["shopping_cart"] as $product){
+    $quantity = $product['quantity'];
+    $productId = $product['productid'];
+    
+
+$sql = "INSERT INTO `orderline` (`idorderline`, 
+                                 `factuurcode`, 
+                                 `productid`,  
+                                 `aantal` 
+                                 ) 
+         VALUES                 (NULL, 
+                                 '$factuurcode', 
+                                 '$productId', 
+                                 '$quantity'
+                                 )";
+                                 
+                                 
+
+
+$result = mysqli_query($con, $sql);
+
 echo $sql;
-
-
 
 $sql2 = "SELECT * FROM product WHERE productid = '$productId'";
 
@@ -54,29 +145,9 @@ $sql2 = "SELECT * FROM product WHERE productid = '$productId'";
       $sql = "UPDATE product SET productaantal = '$af' WHERE product.productid = '$productId'";
       mysqli_query($con,$sql);
 
-      echo '<div class="alert alert-info" role="alert">
-      Bedankt voor uw aankoop. uw product wordt zo spoedig mogelijk op de opgegeven locatie geleverd.</div>';
+}
 
 
 
-    // echo '<meta http-equiv="Refresh" content="1; url=./pdf.php">';
-    
-// price_ex
-// price_inc
-// status
-//var_dump($_SESSION);
-
-//unset($_SESSION['shopping_cart']);
-
-
-
-
-        //echo $sql;
-//var_dump($result);
-
-
-}else{
-    echo '<div class="alert alert-primary" role="alert">login om af te rekenen</div>';
-    echo '<meta http-equiv="Refresh" content="2; url=./index.php?content=login">';
-
+    // echo '<meta http-equiv="Refresh" content="0; url=index.php?content=NAW">';
 }
